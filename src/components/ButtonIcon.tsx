d444caebda93854e0b89a4090210ad98b1aaef9b
yurincmt <me@example.com>
2025-04-29 20:04:11 -0300

    feat: implementação componente ButtonIcon
    * butão de operações matemáticas

---
+++ src/components/ButtonIcon.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, TouchableOpacity } from "react-native"
+import {
+  Plus,
+  Minus,
+  X,
+  Divide,
+  Equals,
+  Backspace,
+  Percent
+} from "phosphor-react-native"
+
+type Props = {
+  iconName: string;
+}
+
+export function ButtonIcon({iconName}:Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      {iconName === 'Plus' && <Plus size={32} weight="bold" />}
+      {iconName === 'Minus' && <Minus size={32} weight="bold" />}
+      {iconName === 'X' && <X size={32} weight="bold" />}
+      {iconName === 'Divide' && <Divide size={32} weight="bold" />}
+      {iconName === 'Equals' && <Equals size={32} weight="bold" />}
+      {iconName === 'Backspace' && <Backspace size={32} weight="bold" />}
+      {iconName === 'Percent' && <Percent size={32} weight="bold" />}
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // borderWidth: 1,
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2
+  }
+})
