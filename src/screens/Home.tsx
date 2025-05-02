7e391c9ea075f148b0f7799ccb73e830b1f11427
yurincmt <me@example.com>
2025-05-01 22:45:32 -0300

    fix: useContext deu pau... tirando

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,15 @@
 import { StyleSheet, View } from "react-native";
-import { createContext, useState } from "react";
+import { useState } from "react";
 
 import { Calc } from "../components/Calc";
 import { Keyboard } from "../components/Keyboard";
-
-export const keyboardContext = createContext<string>('');
 
 export function Home() {
   const [keyboardInput, setKeyboardInput] = useState<string>('31337');
   return (
     <View style={style.container}>
-      <keyboardContext.Provider value={keyboardInput}
-      >
-        <Calc />
-
-        <Keyboard />
-      </keyboardContext.Provider>
+      <Calc currentValue="31337"/>
+      <Keyboard />
     </View>
   )
 }
