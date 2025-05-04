5bdcc03320886df6fc33eaa7637f3dedbbfe56af
yurincmt <me@example.com>
2025-05-04 14:18:37 -0300

    feat: criação componente Icon para usar icon de operadores que não sejam números

---
+++ src/components/Icon.tsx
@@ -0,0 +1,27 @@
+import {
+  Plus,
+  Minus,
+  X,
+  Divide,
+  Equals,
+  Backspace,
+  Percent
+} from "phosphor-react-native"
+
+type Props = {
+  name: string;
+}
+
+export function Icon({name}: Props) {
+  return (
+    <>
+      {name === 'Plus' && <Plus size={32} weight="bold" />}
+      {name === 'Minus' && <Minus size={32} weight="bold" />}
+      {name === 'X' && <X size={32} weight="bold" />}
+      {name === 'Divide' && <Divide size={32} weight="bold" />}
+      {name === 'Equals' && <Equals size={32} weight="bold" />}
+      {name === 'Backspace' && <Backspace size={32} weight="bold" color="#EF7303"/>}
+      {name === 'Percent' && <Percent size={32} weight="bold" />}
+    </>
+  )
+}
