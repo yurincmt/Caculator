74a50dbedfd7c49df2dedb63b28048f452bdedba
yurincmt <me@example.com>
2025-05-04 14:23:58 -0300

    refact: mudança drástica
    * usando apenas um tipo de botão, componente Button
    
    * uso dos utilitários da notação polonesa para calcular expressão inputada pelo usuário

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,40 +1,74 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "./Button";
-import { ButtonIcon } from "./ButtonIcon";
 
-export function Keyboard() {
+import { Button } from "./Button";
+import { infix2Polish, polishExpSolver } from "../utils/polishMath";
+
+interface Props {
+  input: string;
+  setInput: (buttonChar: string) => void;
+  // setInput: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export function Keyboard({input, setInput}: Props) {
+
+  function handlePress(buttonChar: string) {
+    switch (buttonChar) {
+      // case 'number':
+      // case 'operator': 
+      //   setInput(input+buttonChar);
+      //   break;
+
+      case 'clear':
+        setInput('');
+        break;
+
+      case 'del':
+        setInput(input.slice(0, -1));
+        break;
+
+      case '=':
+        const polishExp = infix2Polish(input);
+        setInput(polishExpSolver(polishExp));
+        break;
+      
+      default:
+        setInput(input+buttonChar);
+    }
+  }
+  
   return (
     <View style={styles.container}>
       <View style={styles.column}>
-        <Button charText="C"/>
-        <Button charText="7"/>
-        <Button charText="4"/>
-        <Button charText="1"/>
+        <Button iconName="C" accent onPress={() => handlePress('clear')}/>
+        <Button iconName="7" onPress={() => handlePress('7')}/>
+        <Button iconName="4" onPress={() => handlePress('4')}/>
+        <Button iconName="1" onPress={() => handlePress('1')}/>
       </View>
 
       <View style={styles.column}>
-        <ButtonIcon iconName="Backspace"/>
-        <Button charText="8"/>
-        <Button charText="5"/>
-        <Button charText="2"/>
-        <Button charText="0"/>
+        <Button iconName="Backspace" onPress={() => handlePress('del')}/>
+        <Button iconName="8" onPress={() => handlePress('8')}/>
+        <Button iconName="5" onPress={() => handlePress('5')}/>
+        <Button iconName="2" onPress={() => handlePress('2')}/>
+        <Button iconName="0" onPress={() => handlePress('0')}/>
       </View>
 
       <View style={styles.column}>
-        <ButtonIcon iconName="Percent"/>
-        <Button charText="9"/>
-        <Button charText="6"/>
-        <Button charText="3"/>
-        <Button charText="."/>
+        <Button iconName="Percent" onPress={() => handlePress('%')}/>
+        <Button iconName="9" onPress={() => handlePress('9')}/>
+        <Button iconName="6" onPress={() => handlePress('6')}/>
+        <Button iconName="3" onPress={() => handlePress('3')}/>
+        <Button iconName="." onPress={() => handlePress('.')}/>
       </View>
 
       <View style={styles.column}>
         <View style={styles.opColum}>
-          <ButtonIcon iconName="Divide"/>
-          <ButtonIcon iconName="X"/>
-          <ButtonIcon iconName="Minus"/>
-          <ButtonIcon iconName="Plus"/>
-        <ButtonIcon iconName="Equals"/>
+          <Button iconName="Divide" onPress={() => handlePress('/')}/>
+          <Button iconName="X" onPress={() => handlePress('x')}/>
+          <Button iconName="Minus" onPress={() => handlePress('-')}/>
+          <Button iconName="Plus" onPress={() => handlePress('+')}/>
+          <Button iconName="Equals" onPress={() => handlePress('=')}/>
         </View>
       </View>
     </View>
@@ -43,7 +77,6 @@ export function Keyboard() {
 
 const styles = StyleSheet.create({
   container: {
-    borderWidth: 1,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -55,7 +88,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   opColum: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: '#F5F5F5',
     borderRadius: 18,
     width: '90%',
